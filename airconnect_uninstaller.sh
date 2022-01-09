a81c0c0e3a2b12d549c7ad4bccc575796f94df62
168912916 859421311 <47042125+pheiduck@users.noreply.github.com>
2022-01-09 20:40:26 +0100

    Scripts/os check (#5)
    * [update] add OS Check
    
    Signed-off-by: pheiduck <pheiduck@github.com>
    
    * [fix] (un)installer
    
    Signed-off-by: pheiduck <pheiduck@github.com>
    
    Co-authored-by: pheiduck <pheiduck@github.com>

--- airconnect_uninstaller.sh
+++ airconnect_uninstaller.sh
@@ -7,23 +7,28 @@ if [ $(whoami) != 'root' ]; then
   exit 0
 
 
+# Check OS
+elif [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) != 'raspbian' ]; then
+  echo "You are trying to install this on an unsupported distribution."
+  echo "Only the Raspberry Pi OS is supported."
+
+
 elif [ -d /var/lib/airconnect ]; then
 
-echo "Uninstall AirConnect..."
+  echo "Uninstall AirConnect..."
 
-systemctl disable --now airconnect
+  systemctl disable --now airconnect
 
-rm -rf /var/lib/airconnect
+  rm -rf /var/lib/airconnect
 
-rm /etc/systemd/system/airconnect.service 
+  rm /etc/systemd/system/airconnect.service 
 
-rm /var/log/airupnp.log
-
+  rm /var/log/airupnp.log
 
 else
 	
-echo "AirConnect has already been uninstalled."
-echo "For reinstallation use the installer script."
-exit 0
+  echo "AirConnect has already been uninstalled."
+  echo "For reinstallation use the installer script."
+  exit 0
 
 fi
