0827f434c422d83338174de85860314146c520d3
pheiduck <pheiduck@github.com>
2022-01-09 20:26:12 +0100

    [update] add OS Check
    Signed-off-by: pheiduck <pheiduck@github.com>

--- airconnect_installer.sh
+++ airconnect_installer.sh
@@ -7,50 +7,58 @@ if [ $(whoami) != 'root' ]; then
   exit 0
 
 
+# Check OS
+elif [ $(awk -F= '$1=="ID" { print $2 ;}' /etc/os-release) != 'raspbian' ]; then
+  echo "You are trying to install this on an unsupported distribution."
+  echo "Only the Raspberry Pi OS is supported."
+
+
 elif [ -d /var/lib/airconnect ]; then
 
-echo "Update AirConnect..."
+  echo "Update AirConnect..."
 
-cd /var/lib/airconnect
-rm airupnp-arm
-curl https://raw.githubusercontent.com/philippe44/AirConnect/master/bin/airupnp-arm -o airupnp-arm
-chmod 755 airupnp-arm
+  cd /var/lib/airconnect
+  rm airupnp-arm
+  curl https://raw.githubusercontent.com/philippe44/AirConnect/master/bin/airupnp-arm -o airupnp-arm
+  chmod 755 airupnp-arm
 
-systemctl restart airconnect
+  systemctl restart airconnect
 
-systemctl status airconnect
+  systemctl status airconnect
 
 
 else
 
-echo "Install AirConnect..."
+  echo "Install AirConnect..."
 
-mkdir /var/lib/airconnect
-cd /var/lib/airconnect
-curl https://raw.githubusercontent.com/philippe44/AirConnect/master/bin/airupnp-arm -o airupnp-arm
-chmod 755 airupnp-arm
+  mkdir /var/lib/airconnect
+  cd /var/lib/airconnect
+  curl https://raw.githubusercontent.com/philippe44/AirConnect/master/bin/airupnp-arm -o airupnp-arm
+  chmod 755 airupnp-arm
 
-cd /etc/systemd/system/
-cat > airconnect.service <<EOF
-[Unit]
-Description=AirUPnP bridge
-After=network-online.target
-Wants=network-online.target
+  cd /etc/systemd/system/
+  cat > airconnect.service <<EOF
+  [Unit]
+  Description=AirUPnP bridge
+  After=network-online.target
+  Wants=network-online.target
 
-[Service]
-Type=forking
-ExecStart=/var/lib/airconnect/airupnp-arm -l 1000:2000 -z -f /var/log/airupnp.log
-Restart=on-failure
-RestartSec=30
+  [Service]
+  Type=forking
+  ExecStart=/var/lib/airconnect/airupnp-arm -l 1000:2000 -z -f /var/log/airupnp.log
+  Restart=on-failure
+  RestartSec=30
 
-[Install]
-WantedBy=multi-user.target
+  [Install]
+  WantedBy=multi-user.target
 EOF
 
-cd /var/lib/airconnect
+  cd /var/lib/airconnect
 
-systemctl enable --now airconnect
+  systemctl enable --now airconnect
 
-systemctl status airconnect
+  systemctl status airconnect
 
 fi
+
+
